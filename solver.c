69266e89f4a6be809c4ab2bfcf980d74268392c4
davideprioreschi <105057661+davideprioreschi@users.noreply.github.com>
2022-12-02 14:41:03 +0100

    Update solver.c

--- solver.c
+++ solver.c
@@ -1,20 +1,24 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "problems.h"
 bool check(unsigned char (*problem)[9][9]){
     //Controllo la riga
     int riga, colonna, num;
-    for num=1; num<=9; num=num+1{
-        for riga=0; riga<9; riga=riga+1{
-            for int x=0; x<9; x=x+1{
-                if (*problem)[riga][x]==num{
+    for (num=1; num<=9; num=num+1){
+        for (riga=0; riga<9; riga=riga+1){
+            for (int x=0; x<9; x=x+1){
+                if ((*problem)[riga][x]==num){
                     return 0;
                 }
             }
         }
     }
     //Controllo la colonna
-    for num=1; num<=9; num=num+1{
-        for colonna=0; colonna<9; colonna=colonna+1{
-            for int y=0; y<9; y=y+1{
-                if (*problem)[y][colonna]==num{
+    for (num=1; num<=9; num=num+1){
+        for (colonna=0; colonna<9; colonna=colonna+1){
+            for (int y=0; y<9; y=y+1){
+                if ((*problem)[y][colonna]==num){
                     return 0;
                 }
             }
@@ -22,10 +26,10 @@ bool check(unsigned char (*problem)[9][9]){
     }
     //Controllo il box 3x3
     int rigaBox=riga-riga%3, colonnaBox=colonna-colonna%3;
-    for num=1; num<=9; num=num+1{
-        for riga=0; riga<3; riga=riga+1{
-            for colonna=0; colonna<3; colonna=colonna+1{
-                if (*problem)[riga+rigaBox][colonna+colonnaBox]==num{
+    for (num=1; num<=9; num=num+1){
+        for (riga=0; riga<3; riga=riga+1){
+            for (colonna=0; colonna<3; colonna=colonna+1){
+                if ((*problem)[riga+rigaBox][colonna+colonnaBox]==num){
                     return 0;
                 }
             }
@@ -36,10 +40,11 @@ bool check(unsigned char (*problem)[9][9]){
 void solve(unsigned char (*problem)[9][9])
 {
     int riga, colonna, num;
-    if check==true{
-
+    if (check==true){
+        printf("OK");
     }
-
-
-  
+    else{
+        printf("KO");
+    }
+    //ciaooo
 }
