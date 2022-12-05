f2bbe42d136a82372158848478fa438bd22e8d7e
davideprioreschi <105057661+davideprioreschi@users.noreply.github.com>
2022-12-05 17:41:43 +0100

    Update solver.c

--- src/solver.c
+++ src/solver.c
@@ -1,10 +1,16 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include "problems.h"
-bool check_riga(unsigned char (*problem)[9][9], int riga, int num, int colonna){
+void solve(unsigned char (*problem)[9][9]);
+bool risoluzione(unsigned char (*problem)[9][9]);
+bool check_riga(unsigned char (*problem)[9][9], int num, int riga);
+bool check_colonna(unsigned char (*problem)[9][9], int num, int colonna);
+bool check_box(unsigned char (*problem)[9][9], int num, int colonna, int riga);
+bool numero_vuoto(unsigned char (*problem)[9][9],int *riga, int *colonna);
+
+bool check_riga(unsigned char (*problem)[9][9], int num, int riga){
     //Controlla la riga
-    for (riga=0; riga<9; riga=riga+1){
+    for (int colonna=0; colonna<9; colonna=colonna+1){
             if ((*problem)[riga][colonna]==num){
                 return 1;
             }
@@ -12,9 +18,9 @@ bool check_riga(unsigned char (*problem)[9][9], int riga, int num, int colonna){
     return 0;
 }
 
-bool check_colonna(unsigned char (*problem)[9][9], int colonna, int num, int riga){
+bool check_colonna(unsigned char (*problem)[9][9], int num,int colonna){
     //Controlla la colonna
-    for (colonna=0; colonna<9; colonna=colonna+1){
+    for (int riga=0; riga<9; riga=riga+1){
             if ((*problem)[riga][colonna]==num){
                 return 1;
             }
@@ -22,19 +28,18 @@ bool check_colonna(unsigned char (*problem)[9][9], int colonna, int num, int rig
     return 0;
 }
 
-bool check_box(unsigned char (*problem)[9][9], int colonna, int num, int riga){
+bool check_box(unsigned char (*problem)[9][9], int num, int colonna, int riga){
     //Controllo il box 3x3
     int rigaBox=riga-riga%3;
     int colonnaBox=colonna-colonna%3;
-    for (num=1; num<=9; num=num+1){
-        for (int i=0; i<3; i=i+1){
+    for (int i=0; i<3; i=i+1){
             for (int j=0; j<3; j=j+1){
                 if ((*problem)[i+rigaBox][j+colonnaBox]==num){
                     return 1;
                 }
             }
         }
-    }
+    
     return 0;
 }
 
@@ -46,6 +51,7 @@ bool numero_vuoto(unsigned char (*problem)[9][9],int *riga, int *colonna){
             if ((*problem)[i][j]==0){
                 *riga=i;
                 *colonna=j;
+                
                 return 1;
             }
         }
@@ -53,25 +59,25 @@ bool numero_vuoto(unsigned char (*problem)[9][9],int *riga, int *colonna){
     return 0;
 }
 
-int risoluzione(unsigned char (*problem)[9][9]){
+bool risoluzione(unsigned char (*problem)[9][9]){
         int riga;
         int colonna;
-        if(!numero_vuoto(problem, &riga, &colonna)){
-            int num; 
-            for (num=1;num<=9;num=num+1){
-                if (check_riga(problem,riga, num, colonna) && check_colonna(problem,colonna,num, riga) && check_box(problem,colonna,num,riga)){
+        
+        if(numero_vuoto(&(*problem), &riga, &colonna)){
+            for(int num=1;num<=9;num=num+1){
+                if (!check_riga(&(*problem),num, riga) && !check_colonna(&(*problem),num, colonna) && !check_box(&(*problem), num, colonna,riga)){
+                    //printf("%d,%d \n", riga, colonna);
                     (*problem)[riga][colonna]=num;
-                    if (risoluzione(problem)){
+                    //printf("%d \n",(*problem)[riga][colonna]=num);
+                    if (risoluzione(&(*problem))){
                         return 1;
+                    }else{
+                        (*problem)[riga][colonna]=0;
+                        //printf("%d \n",(*problem)[riga][colonna]=num);
                     }
-                    (*problem)[riga][colonna]=0;
                 }
             }
         }
         return 0;
 
-        }
-
-void solve(unsigned char (*problem)[9][9]){
-    risoluzione(problem);
 }
